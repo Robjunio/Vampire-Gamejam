536010f84567fcef9b9efef96d2be7b0a98b4ce2
Paulo Gustavo Lopes Andrade <gustavoo.magia@gmail.com>
2024-10-17 15:56:07 -0400

    Garlic and Holy Water Implementation
    Implemented the garlic item, which deals damage within an area it's placed. Also implemented the holy water bottle, which is thrown automatically and deals damage to an area when it explodes. Fixed the stakes' rotation when thrown at enemies.

--- Vampire_like-Game/Assets/Scripts/WeaponController.cs
+++ Vampire_like-Game/Assets/Scripts/WeaponController.cs
@@ -11,21 +11,39 @@ public class WeaponController : MonoBehaviour
     [SerializeField] private float projectileSpeed;
     [SerializeField] private StakeProjectile projectilePrefab;
 
+    [SerializeField] private float garlicCoolDownTimer;
+    [SerializeField] private float currentGarlicAttackTime;
+    [SerializeField] private bool isGarlicInUse;
+
+    [SerializeField] private GarlicProjectile garlicProjectilePrefab;
+
+    [SerializeField] private float holyWaterCoolDownTimer;
+    [SerializeField] private float currentHolyWaterAttackTime;
+    [SerializeField] private bool isHolyWaterInUse;
+
+    [SerializeField] private float holyWaterProjectileSpeed;
+    [SerializeField] private HolyWaterProjectile holyWaterProjectilePrefab;
+
     void Start()
     {
         isStakeAttacking = false;
+        isGarlicInUse = false;
+        currentHolyWaterAttackTime = holyWaterCoolDownTimer;
+        isHolyWaterInUse = true;
     }
 
     void Update()
     {
         StakeAttackController();
+        GarlicAttackController();
+        HolyWaterAttackController();
     }
 
     private void StakeAttackController()
     {
         if (!isStakeAttacking)
         {
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetKeyDown(KeyCode.E))
             {
                 isStakeAttacking = true;
                 StakeAttack();
@@ -48,11 +66,75 @@ public class WeaponController : MonoBehaviour
 
     private void StakeAttack()
     {
-        Vector3 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        Vector2 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
 
         var position = transform.position + transform.forward;
-        var rotation = transform.rotation;
+        float lookAngle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        var rotation = Quaternion.Euler(0, 0, lookAngle);
+
         var projectile = Instantiate(projectilePrefab, position, rotation);
         projectile.Fire(projectileSpeed, dir);
+    }
+
+    private void GarlicAttackController()
+    {
+        if (!isGarlicInUse)
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                isGarlicInUse = true;
+                GarlicAttack();
+                currentGarlicAttackTime = garlicCoolDownTimer;
+            }
+        }
+        else
+        {
+            if (currentGarlicAttackTime > 0)
+            {
+                currentGarlicAttackTime -= Time.deltaTime;
+            }
+            else
+            {
+                isGarlicInUse = false;
+            }
+        }
+    }
+
+    private void GarlicAttack()
+    {
+        var position = transform.position + transform.forward;
+
+        var projectile = Instantiate(garlicProjectilePrefab, position, transform.rotation);
+    }
+
+    private void HolyWaterAttackController()
+    {
+        if (!isHolyWaterInUse)
+        {
+            isHolyWaterInUse = true;
+            HolyWaterAttack();
+            currentHolyWaterAttackTime = holyWaterCoolDownTimer;
+        }
+        else
+        {
+            if (currentHolyWaterAttackTime > 0)
+            {
+                currentHolyWaterAttackTime -= Time.deltaTime;
+            }
+            else
+            {
+                isHolyWaterInUse = false;
+            }
+        }
+    }
+
+    private void HolyWaterAttack()
+    {
+        Vector2 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+        var position = transform.position + transform.forward;
+
+        var projectile = Instantiate(holyWaterProjectilePrefab, position, transform.rotation);
+        projectile.Fire(holyWaterProjectileSpeed, dir);
     }
 }
